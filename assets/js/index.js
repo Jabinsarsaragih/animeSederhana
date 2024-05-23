925de08f7f9d55d2be40dd25647e21b2c4e1dbeb
Jabinsar Saragih <jabinsarsaragih@gmail.com>
2024-05-23 14:41:01 +0700

    fix : Memperbaiki Kerusakan

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,6 +11,37 @@ item.forEach(link => {
         indicator(e.target);
     })
 })
-
-
 // TODO End Link Indicator
+
+// TODO Scrol Top
+
+let toTop = document.getElementById('toTop');
+toTop.style.display = 'none';
+window.addEventListener('scroll', () => {
+    if (this.scrollY > 500) {
+        toTop.style.display = 'block';
+    } else {
+        toTop.style.display = 'none';
+    }
+})
+toTop.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+// TODO End Scrol Top
+
+// TODO Start Form
+
+function Login() {
+        var email = document.getElementById("email").value;
+        var pass = document.getElementById("password").value;
+        if (email === "jabinsarsaragih@gmail.com" && pass === "123") {
+            alert("Selamat anda berhasil login");
+        } else {
+            alert("Username dan password anda salah!");
+        }
+    }
+
+// TODO End Form
